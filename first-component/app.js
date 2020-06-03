a23285eff144b9b281a886f56a1bbffe189c2acd
c-champagne <cassielchampagne@gmail.com>
2020-06-02 20:16:27 -0400

    Completed first-component exercise

--- first-component/app.js
+++ first-component/app.js
@@ -1,5 +1,15 @@
 let FirstComponent = () => {
-    return <span>Make this component render like the Windows Logo: <a href="https://goo.gl/images/mY5Qdv" target="_blank">https://goo.gl/images/mY5Qdv</a></span>
+    return <div>
+                <div className="boxContainer">
+                    <div className="redBox"></div>
+                    <div className="greenBox"></div>
+                </div>
+                <div className="boxContainer">
+                    <div className="blueBox"></div>
+                    <div className="yellowBox"></div>
+                </div>
+            </div>
+    //return <span>Make this component render like the Windows Logo: <a href="https://goo.gl/images/mY5Qdv" target="_blank">https://goo.gl/images/mY5Qdv</a></span>
 }
 
 ReactDOM.render(<FirstComponent />, document.getElementById('root'))
