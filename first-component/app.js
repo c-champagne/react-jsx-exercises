2382833b8bb1a775a24514158992b6d134ed2a31
c-champagne <cassielchampagne@gmail.com>
2020-06-02 20:22:19 -0400

    Added notes to solution for first-component

--- first-component/app.js
+++ first-component/app.js
@@ -1,14 +1,14 @@
 let FirstComponent = () => {
-    return <div>
-                <div className="boxContainer">
-                    <div className="redBox"></div>
+    return <div> {/* You can only have one parent element in JSX.  So everything needs to go within THIS div. */}
+                <div className="boxContainer"> {/* This is the container to hold the top two boxes. */}
+                    <div className="redBox"></div> {/* Each div is a box with a class named after the color I'd like it to be. */}
                     <div className="greenBox"></div>
                 </div>
-                <div className="boxContainer">
+                <div className="boxContainer"> {/* This is the container to hold the bottom two boxes. */}
                     <div className="blueBox"></div>
                     <div className="yellowBox"></div>
                 </div>
-            </div>
+            </div> /*  End of the parent div from line 2 */
     //return <span>Make this component render like the Windows Logo: <a href="https://goo.gl/images/mY5Qdv" target="_blank">https://goo.gl/images/mY5Qdv</a></span>
 }
 
