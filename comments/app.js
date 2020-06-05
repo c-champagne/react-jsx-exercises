007cd5913322020f91c68712f984bd5093703210
c-champagne <cassielchampagne@gmail.com>
2020-06-04 21:27:07 -0400

    Finished comments exercise

--- comments/app.js
+++ comments/app.js
@@ -1,17 +1,34 @@
 let ProfilePic = () => {
-    return <span>Make this component render an image</span>
+    return <img style={{width:100}} src="simoncute.jpg"/>
+    
+    //<span>Make this component render an image</span>
 }
 
 let CommentBody = () => {
-    return <span>Make this component render a username in bold above the comment text</span>
+    return <div>
+        <h3>Simon</h3>
+        <p>Woof woof.</p>
+        </div>
+    //<span>Make this component render a username in bold above the comment text</span>
 }
 
 let Comment = () => {
-    return <span>Make each comment render a profile picture and the comment body</span>
+    return <div>
+    <ProfilePic/><CommentBody/>
+    </div>
+
+    //<span>Make each comment render a profile picture and the comment body</span>
 }
 
 let Comments = () => {
-    return <span>Make this component render a list of comments</span>
+    return <div>
+    <Comment/>
+    <Comment/>
+    <Comment/>
+    <Comment/>
+    <Comment/>
+    </div>
+    //<span>Make this component render a list of comments</span>
 }
 
 ReactDOM.render(<Comments />, document.getElementById('root'))
