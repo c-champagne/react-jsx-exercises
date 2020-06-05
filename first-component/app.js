df7d1b3095b891a3a5da907f40f43f6fbf56a05e
c-champagne <cassielchampagne@gmail.com>
2020-06-04 21:08:55 -0400

    Completed form exercise

--- first-component/app.js
+++ first-component/app.js
@@ -1,3 +1,20 @@
+//Variable form
+
+let newComponent =
+     <div> {/* You can only have one parent element in JSX.  So everything needs to go within THIS div. */}
+                <div className="boxContainer"> {/* This is the container to hold the top two boxes. */}
+                    <div className="redBox"></div> {/* Each div is a box with a class named after the color I'd like it to be. */}
+                    <div className="greenBox"></div>
+                </div>
+                <div className="boxContainer"> {/* This is the container to hold the bottom two boxes. */}
+                    <div className="blueBox"></div>
+                    <div className="yellowBox"></div>
+                </div>
+            </div> /*  End of the parent div from line 2 */
+    //return <span>Make this component render like the Windows Logo: <a href="https://goo.gl/images/mY5Qdv" target="_blank">https://goo.gl/images/mY5Qdv</a></span>
+
+//Function form
+
 let FirstComponent = () => {
     return <div> {/* You can only have one parent element in JSX.  So everything needs to go within THIS div. */}
                 <div className="boxContainer"> {/* This is the container to hold the top two boxes. */}
@@ -12,4 +29,7 @@ let FirstComponent = () => {
     //return <span>Make this component render like the Windows Logo: <a href="https://goo.gl/images/mY5Qdv" target="_blank">https://goo.gl/images/mY5Qdv</a></span>
 }
 
-ReactDOM.render(<FirstComponent />, document.getElementById('root'))
+ReactDOM.render(newComponent, document.getElementById('root'))
+
+//Function form
+// ReactDOM.render(<FirstComponent />, document.getElementById('root'))
--- form/app.js
+++ form/app.js
@@ -1,17 +1,36 @@
 let TextInput = () => {
-    return <span>Make this component render a styled text input tag</span>
+    return <input className="textBox" type="text"></input>
+    //return <span>Make this component render a styled text input tag</span>
 }
 
 let YesNoRadio = () => {
-    return <span>Make this component render two styled radio button side by side labelled "yes" and "no"</span>
+    return <div>
+        <input className="radioButtons" type="radio" id="yes" name="yesRadio" value="yes"></input>
+        <label htmlFor="yes" className="radioButtons">Yes</label><br></br>
+        <input className="radioButtons" type="radio" id="no" name="noRadio" value="no"></input>
+        <label htmlFor="no" className="radioButtons">No</label>
+</div>
+    //<span>Make this component render two styled radio button side by side labelled "yes" and "no"</span>
 }
 
 let SubmitButton = () => {
-    return <span>Make this component render a styled button of type "submit"</span>
+    return <button className="theButton" type="submit">Click me!</button>
+    //<span>Make this component render a styled button of type "submit"</span>
 }
 
 let Form = () => {
-    return <span>Make this Component render a form tag with 3 text inputs, two yes/no radio buttons, and a submit button </span>
+return <div>
+    <form>
+        <TextInput/>
+        <TextInput/>
+        <YesNoRadio/>
+        <TextInput/>
+        <YesNoRadio/>
+        <SubmitButton/>
+    </form>
+</div>
+    
+    //return <span>Make this Component render a form tag with 3 text inputs, two yes/no radio buttons, and a submit button </span>
 }
 
 
